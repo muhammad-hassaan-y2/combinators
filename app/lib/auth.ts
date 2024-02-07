aba306ea7945063bd3e13b8b93fe14f4875c6155
Muhammad Hassaan Shaukat <mr.hassaan50@gmail.com>
2024-02-07 20:35:04 +0500

    Authentication Setup

---
+++ app/lib/auth.ts
@@ -0,0 +1,20 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { NextAuthOptions } from "next-auth";
+import Github from "next-auth/providers/github";
+import GithubProvider from 'next-auth/providers/github'
+
+const prisma = new PrismaClient
+
+export const authOptions: NextAuthOptions = {
+    // @ts-ignore
+    adapter: PrismaAdapter(prisma),
+
+        providers: [
+            GithubProvider({
+                clientId: process.env.GITHUB_CLIENT_ID as string,
+                clientSecret: process.env.GITHUB_SECRET_ID as string
+            })
+        ]
+}
+
